0e59dd396925041c4bf2e1ea939bf93ecebf1b43
Anick Saha <anicksaha05@gmail.com>
2019-11-06 14:05:02 -0600

    Working state 01

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from utils_io import read_fasta_file
-from utils_io import write_distance_file
-from utils import get_difference_matrix
+import utils_io as IO
+import utils
+from neighbor_joining import nei_saitou
 import sys
 
 def main(filename):
     # read the fna file and return the ids and id sequence mappings
-    ids, id_sequences = read_fasta_file(filename)
+    ids, id_sequences = IO.read_fasta_file(filename)
 
     # generate the difference matrix
-    distance_matrix = get_difference_matrix(ids, id_sequences)
+    distance_matrix = utils.get_difference_matrix(ids, id_sequences)
 
     # write the distances.txt file (distance matrix)
-    write_distance_file(ids, distance_matrix)
+    IO.write_distance_file(ids, distance_matrix)
+
+    # run the nei saitu algorithm. Returns the root of the tree
+    root = nei_saitou(ids, distance_matrix)
+
+    # write the edge file using preorder traversal
+    IO.write_edge_file(root)
+
+    # write the newick file using postorder traversal
+    IO.write_newick_file(ids, root)
+
+    # bootstrap bonus points
+    percentages = utils.bootstrap(root, ids, id_sequences)
+    IO.write_bootstrap(percentages)
 
 if __name__ == '__main__':
     main(sys.argv[1])
--- utils_io.py
+++ utils_io.py
@@ -21,3 +21,53 @@ def write_distance_file(ids, matrix):
         f.write('\t' + '\t'.join(ids) + '\n')
         for i in xrange(count):
             f.write(ids[i] + '\t' + '\t'.join(map(str, matrix[i])) + '\n')
+
+
+# uses preorder traversal to traverse the tree and generate the edges file
+def write_edge_file(root):
+    traversal = []
+    # recursive preorder traversal
+    def preorder_traversal(node):
+        if not node or not node.children:
+            return
+        for child, distance in node.children.items():
+            # traverse root first
+            traversal.append((node.id, child.id, distance))
+            # then traverse children recursively
+            preorder_traversal(child)
+    preorder_traversal(root)
+    # writes the traversal list to file
+    with open('edges.txt', 'w') as f:
+        for (parent, child, distance) in traversal:
+            f.write(parent + '\t' + child + '\t' + str(distance) + '\n')
+
+
+# uses post order traversal to write the newick file
+def write_newick_file(original_ids, root):
+    # recursive postorder traversal
+    def postorder_traversal(node):
+        if not node.children:
+            # if it a leaf node then return the original id
+            if 1 <= int(node.id) <= 61:
+                return original_ids[int(node.id) - 1]
+        vals = []
+        # recursively traverse the children first
+        for child, distance in node.children.items():
+            vals.append(postorder_traversal(child) + ':' + str(distance))
+        result = '(' + ','.join(vals) + ')'
+        return result
+    # get the root nodes children.
+    # split it up into a 2 pair and 1 extra node for the root
+    (first, fd), (second, sd), (third, td) = root.children.items()
+    # recursively call the postorder traversal function to generate the newick format
+    first_part = '(' + postorder_traversal(second) + ':' + str(sd) + ','+ postorder_traversal(third) + ':' + str(td) + ')'
+    newick =  '(' + first_part + ':' + str(fd) + ');'
+    with open('tree.txt', 'w') as f:
+        f.write(newick)
+
+
+# write the percentages in the bootstrap file
+def write_bootstrap(percentages):
+    with open('bootstrap.txt', 'w') as f:
+        for percent in percentages:
+            f.write(str(percent) + '\n')
