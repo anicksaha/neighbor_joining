800e390143d4b47b2eb67c09a49968c704de7784
Anick Saha <anicksaha05@gmail.com>
2019-11-07 01:22:22 -0600

    Checkpoint 11/07 01:22

--- neighbour_joining.py
+++ neighbour_joining.py
@@ -2,7 +2,7 @@ from node import Node
 import utils_nj as utils
 
 # Applies nei_saitou to calculte the distances recursively and get the phylogeny tree
-def nei_saitou(original_ids, original_distMatrix, seqCounter):
+def nei_saitou(original_ids, original_dist_matrix, sequence_counter):
     #Using a global root holder
     global root
     root = None
@@ -14,9 +14,9 @@ def nei_saitou(original_ids, original_distMatrix, seqCounter):
     for i, id in enumerate(original_ids):
         seqId_orderId_map[id] = str(i + 1)
     
-    def calculate_next_neighbours(ids, distMatrix, seqCounter, seqId_orderId_map):
+    def calculate_next_neighbours(ids, dist_matrix, sequence_counter, seqId_orderId_map):
         global root
-        N  = len(distMatrix)
+        N  = len(dist_matrix)
 
         # Base case for recursion
         if N <= 2:
@@ -30,15 +30,15 @@ def nei_saitou(original_ids, original_distMatrix, seqCounter):
             else:
                 tip_2 = ids[1]
             # Adding their relationship to the tree
-            relationship_map[tip_1] = (tip_2, distMatrix[0][1])
+            relationship_map[tip_1] = (tip_2, dist_matrix[0][1])
             # Since these are the last two nodes, any of these can be chosen to be the root
             root = tip_2
             return
 
         #First step is to get the closest two tips from the distance matrix
-        mini, minj, minVal = utils.construct_QMatrix(distMatrix)
+        mini, minj, minVal = utils.get_qmatrix(dist_matrix)
         
-        edge_i, edge_j = utils.calculate_edge_lengths(distMatrix, mini, minj, N)
+        edge_i, edge_j = utils.calculate_edge_lengths(dist_matrix, mini, minj, N)
         #print(edge_i, edge_j)
         tip_1 = tip_2 = None
         # Setting the new ids to the tips/ internal node
@@ -52,19 +52,19 @@ def nei_saitou(original_ids, original_distMatrix, seqCounter):
             tip_2 = ids[minj]
         
         # Add them to the tree relationships
-        relationship_map[tip_1] = (str(seqCounter), edge_i)
-        relationship_map[tip_2] = (str(seqCounter), edge_j)
+        relationship_map[tip_1] = (str(sequence_counter), edge_i)
+        relationship_map[tip_2] = (str(sequence_counter), edge_j)
 
-        # Remove the used ids and add the new seqCounter
-        ids = [id for id in ids if id not in (ids[mini], ids[minj])] + [str(seqCounter)]
+        # Remove the used ids and add the new sequence_counter
+        ids = [id for id in ids if id not in (ids[mini], ids[minj])] + [str(sequence_counter)]
 
-        seqCounter -=1
-        new_distMatrix = utils.calculate_new_distMatrix(distMatrix, mini, minj, N)
+        sequence_counter -=1
+        new_dist_matrix = utils.get_new_dist_matrix(dist_matrix, mini, minj, N)
 
         # recursively call with the new distance matrix
-        calculate_next_neighbours(ids, new_distMatrix, seqCounter, seqId_orderId_map)
+        calculate_next_neighbours(ids, new_dist_matrix, sequence_counter, seqId_orderId_map)
 
-    calculate_next_neighbours(original_ids, original_distMatrix, seqCounter, seqId_orderId_map)
+    calculate_next_neighbours(original_ids, original_dist_matrix, sequence_counter, seqId_orderId_map)
 
     # construct tree from the relationship_map relations
     rootNode = Node(root)
--- utils_nj.py
+++ utils_nj.py
@@ -2,12 +2,9 @@
 # used by the nei-saitou neighbor-joining algorithm
 
 # Calculates the Q matrix from the distance matrix and returns the two closest nodes and their distance
-def construct_QMatrix(distance_matrix):
+def get_qmatrix(distance_matrix):
     N = len(distance_matrix)
-    # Matrix to store the Q values
     QMatrix = [[0] * N for _ in range(N)]
-
-    # We also want to return the two closest nodes
     # keep track of the minimum value and indices in the Q matrix
     mini = minj = 0
     minVal = float('inf')
@@ -17,7 +14,6 @@ def construct_QMatrix(distance_matrix):
             if i == j:
                 continue
             QMatrix[i][j] = (N - 2) * distance_matrix[i][j] - sum(distance_matrix[i]) - sum(distance_matrix[j])
-            
             if QMatrix[i][j] < minVal:
                 mini = i
                 minj = j
@@ -26,26 +22,28 @@ def construct_QMatrix(distance_matrix):
     return mini, minj, minVal
 
 # Calculates the edge lengths to the u node and returns the lengths.
-def calculate_edge_lengths(distMatrix, mini, minj, N):
+def calculate_edge_lengths(dist_matrix, mini, minj, N):
     # Distances to the new internal node
-    edge_i = 1/ 2.0 * distMatrix[mini][minj] + 1 / (2.0  * (N - 2))* (sum(distMatrix[mini]) - sum(distMatrix[minj]))
+    edge_i = 1/ 2.0 * dist_matrix[mini][minj] + 1 / (2.0  * (N - 2))* (sum(dist_matrix[mini]) - sum(dist_matrix[minj]))
     # distance from minj node to u
-    edge_j = distMatrix[mini][minj] - edge_i
+    edge_j = dist_matrix[mini][minj] - edge_i
     return edge_i, edge_j
 
 
-def calculate_new_distMatrix(distMatrix, mini, minj, N):
-    updatedDistances = [[0] * (N + 1) for _ in range(N + 1)]
+def get_new_dist_matrix(dist_matrix, mini, minj, N):
+    
+    updated_distances = [[0] * (N + 1) for _ in range(N + 1)]
     for i in xrange(N):
         for j in xrange(N):
-            updatedDistances[i][j] = distMatrix[i][j]
+            updated_distances[i][j] = dist_matrix[i][j]
+    
     # update the distances to the new node
     for k in range(N):
-        updatedDistances[N][k] = (0.5) * (distMatrix[mini][k] + distMatrix[minj][k] - distMatrix[mini][minj])
-        updatedDistances[k][N] = updatedDistances[N][k]
+        updated_distances[N][k] = (0.5) * (dist_matrix[mini][k] + dist_matrix[minj][k] - dist_matrix[mini][minj])
+        updated_distances[k][N] = updated_distances[N][k]
 
-    # Creates a new distance matrix 
-    new_distMatrix = [[0] * (N - 1) for _ in range(N - 1)]
+    # Create a new distance matrix 
+    new_dist_matrix = [[0] * (N - 1) for _ in range(N - 1)]
     keep_i = keep_j = 0
     for i in range(N + 1):
         # Replacing these two with a new node
@@ -56,8 +54,8 @@ def calculate_new_distMatrix(distMatrix, mini, minj, N):
             # Replacing these two with the new node
             if j == mini or j == minj:
                 continue
-            new_distMatrix[keep_i][keep_j] = updatedDistances[i][j]
+            new_dist_matrix[keep_i][keep_j] = updated_distances[i][j]
             keep_j += 1
         keep_i += 1
 
-    return new_distMatrix
+    return new_dist_matrix
