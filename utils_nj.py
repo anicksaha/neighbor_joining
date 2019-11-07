52ac94a0823aaa4af3d3967d28ebfd3ebbfdb02d
Anick Saha <anicksaha05@gmail.com>
2019-11-07 01:38:56 -0600

    Amend.

--- utils_nj.py
+++ utils_nj.py
@@ -1,42 +1,36 @@
-# This file contains the util functions 
-# used by the nei-saitou neighbor-joining algorithm
+## This file contains the util functions used by the nei-saitou neighbor-joining algorithm
 
 # Calculates the Q matrix from the distance matrix and returns the two closest nodes and their distance
-def get_qmatrix(distance_matrix):
-    N = len(distance_matrix)
-    QMatrix = [[0] * N for _ in range(N)]
+def get_qmatrix(dist_matrix):
+    N = len(dist_matrix)
+    qmatrix = [[0] * N for _ in range(N)]
     # keep track of the minimum value and indices in the Q matrix
-    mini = minj = 0
-    minVal = float('inf')
-
+    min_i = min_j = 0
+    min_val = float('inf')
     for i in range(N):
         for j in range(N):
             if i == j:
                 continue
-            QMatrix[i][j] = (N - 2) * distance_matrix[i][j] - sum(distance_matrix[i]) - sum(distance_matrix[j])
-            if QMatrix[i][j] < minVal:
-                mini = i
-                minj = j
-                minVal = QMatrix[i][j]
-
-    return mini, minj, minVal
+            qmatrix[i][j] = (N - 2) * dist_matrix[i][j] - sum(dist_matrix[i]) - sum(dist_matrix[j])
+            if qmatrix[i][j] < min_val:
+                min_i = i
+                min_j = j
+                min_val = qmatrix[i][j]
+    return min_i, min_j, min_val
 
 # Calculates the edge lengths to the u node and returns the lengths.
-def calculate_edge_lengths(dist_matrix, mini, minj, N):
+def calculate_edge_lengths(dist_matrix, min_i, min_j, N):
     # Distances to the new internal node
-    edge_i = 1/ 2.0 * dist_matrix[mini][minj] + 1 / (2.0  * (N - 2))* (sum(dist_matrix[mini]) - sum(dist_matrix[minj]))
-    # distance from minj node to u
-    edge_j = dist_matrix[mini][minj] - edge_i
+    edge_i = 1/ 2.0 * dist_matrix[min_i][min_j] + 1 / (2.0  * (N - 2))* (sum(dist_matrix[min_i]) - sum(dist_matrix[min_j]))
+    # distance from min_j node to u
+    edge_j = dist_matrix[min_i][min_j] - edge_i
     return edge_i, edge_j
 
-
 def get_new_dist_matrix(dist_matrix, mini, minj, N):
-    
     updated_distances = [[0] * (N + 1) for _ in range(N + 1)]
     for i in xrange(N):
         for j in xrange(N):
             updated_distances[i][j] = dist_matrix[i][j]
-    
     # update the distances to the new node
     for k in range(N):
         updated_distances[N][k] = (0.5) * (dist_matrix[mini][k] + dist_matrix[minj][k] - dist_matrix[mini][minj])
