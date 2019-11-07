8b5d18eed3babd67a1a46e93482493a567d9a4e0
Anick Saha <anicksaha05@gmail.com>
2019-11-07 16:40:35 -0600

    :)

--- neighbour_joining.py
+++ neighbour_joining.py
@@ -6,7 +6,7 @@ def nei_saitou(original_ids, original_dist_matrix, sequence_counter):
     #Using a global root holder
     global root
     root = None
-    # Maintaining a map for the relationships between child and parent
+    # Maintaining a map for the relationships b/w child and parent
     relationship_map = {}
 
     # Using a mapping for seqId and new ids.
@@ -18,42 +18,40 @@ def nei_saitou(original_ids, original_dist_matrix, sequence_counter):
         global root
         N  = len(dist_matrix)
 
-        # Base case for recursion
-        if N <= 2:
-            # Assigning the ids to these nodes according to the requirements
+        if N <= 2: # Base case
             if ids[0] in seqId_orderId_map:
-                tip_1 = seqId_orderId_map[ids[0]]
+                node_1 = seqId_orderId_map[ids[0]]
             else:
-                tip_1 = ids[0]
+                node_1 = ids[0]
             if ids[1] in seqId_orderId_map:
-                tip_2 = seqId_orderId_map[ids[1]]
+                node_2 = seqId_orderId_map[ids[1]]
             else:
-                tip_2 = ids[1]
+                node_2 = ids[1]
             # Adding their relationship to the tree
-            relationship_map[tip_1] = (tip_2, dist_matrix[0][1])
+            relationship_map[node_1] = (node_2, dist_matrix[0][1])
             # Since these are the last two nodes, any of these can be chosen to be the root
-            root = tip_2
+            root = node_2
             return
 
         #First step is to get the closest two tips from the distance matrix
         mini, minj, minVal = utils.get_qmatrix(dist_matrix)
         
         edge_i, edge_j = utils.calculate_edge_lengths(dist_matrix, mini, minj, N)
-        #print(edge_i, edge_j)
-        tip_1 = tip_2 = None
+        node_1 = node_2 = None
+
         # Setting the new ids to the tips/ internal node
         if ids[mini] in seqId_orderId_map:
-            tip_1 = seqId_orderId_map[ids[mini]]
+            node_1 = seqId_orderId_map[ids[mini]]
         else:
-            tip_1 = ids[mini]
+            node_1 = ids[mini]
         if ids[minj] in seqId_orderId_map:
-            tip_2 = seqId_orderId_map[ids[minj]]
+            node_2 = seqId_orderId_map[ids[minj]]
         else:
-            tip_2 = ids[minj]
+            node_2 = ids[minj]
         
         # Add them to the tree relationships
-        relationship_map[tip_1] = (str(sequence_counter), edge_i)
-        relationship_map[tip_2] = (str(sequence_counter), edge_j)
+        relationship_map[node_1] = (str(sequence_counter), edge_i)
+        relationship_map[node_2] = (str(sequence_counter), edge_j)
 
         # Remove the used ids and add the new sequence_counter
         ids = [id for id in ids if id not in (ids[mini], ids[minj])] + [str(sequence_counter)]
@@ -67,19 +65,18 @@ def nei_saitou(original_ids, original_dist_matrix, sequence_counter):
     calculate_next_neighbours(original_ids, original_dist_matrix, sequence_counter, seqId_orderId_map)
 
     # construct tree from the relationship_map relations
-    rootNode = Node(root)
-    queue = [rootNode]
+    root_node = Node(root)
+    queue = [root_node]
     # bfs for tree construction
     while len(queue) > 0:
-        nodeList = []
+        node_list = []
         for node in queue:
             for child, (parent, distance) in relationship_map.items():
                 if parent == node.id:
                     childNode = Node(child)
                     node.add_child(childNode, distance)
-                    nodeList.append(childNode)
-        for node in nodeList:
+                    node_list.append(childNode)
+        for node in node_list:
             del relationship_map[node.id]
-        queue = nodeList
-    return rootNode
-
+        queue = node_list
+    return root_node
